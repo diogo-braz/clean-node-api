41d2b90a09a5cd4809b2f15d373e24ef1db4623a
Diogo Braz <diogo.braz04@gmail.com>
2024-05-06 07:51:44 -0300

    chore: change no namespace rule to false

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   root: true,
   rules: {
     "no-undef": "off",
+    "@typescript-eslint/no-namespace": "off",
     "@typescript-eslint/no-unused-vars": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/quotes": ["error", "double"],
