939805f92e2c0638f9ffdb93e8f3a43cde1da9dd
Diogo Braz <diogo.braz04@gmail.com>
2024-05-08 20:36:24 -0300

    refactor: move SUT creation

--- tests/utils/email-validator-adapter.spec.ts
+++ tests/utils/email-validator-adapter.spec.ts
@@ -8,21 +8,24 @@ jest.mock("validator", () => ({
 }));
 
 describe("EmailValidator Adapter", () => {
+  let sut: EmailValidatorAdapter;
+
+  beforeEach(() => {
+    sut = new EmailValidatorAdapter();
+  });
+
   it("should return false if validator returns false", () => {
-    const sut = new EmailValidatorAdapter();
     jest.spyOn(validator, "isEmail").mockReturnValueOnce(false);
     const isValid = sut.isValid("invalid_email@mail.com");
     expect(isValid).toBe(false);
   });
 
   it("should return true if validator returns true", () => {
-    const sut = new EmailValidatorAdapter();
     const isValid = sut.isValid("valid_email@mail.com");
     expect(isValid).toBe(true);
   });
 
   it("should call validator with correct params", () => {
-    const sut = new EmailValidatorAdapter();
     const isEmailSpy = jest.spyOn(validator, "isEmail");
     sut.isValid("any_email@mail.com");
     expect(isEmailSpy).toHaveBeenCalledWith("any_email@mail.com");
