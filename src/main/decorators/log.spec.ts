263f0d5a16961edf0028a6accfd18382b74cb7ee
Diogo Braz <diogo.braz04@gmail.com>
2024-05-20 00:10:48 -0300

    test: ensure LogControllerDecorator calls controller with correct value

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,29 @@
+import { Controller } from "@/presentation/protocols";
+import { LogControllerDecorator } from "./log";
+
+import { mock, MockProxy } from "jest-mock-extended";
+
+describe("LogController Decorator", () => {
+  let sut: LogControllerDecorator;
+  let controllerStub: MockProxy<Controller>;
+
+  beforeEach(() => {
+    controllerStub = mock();
+    controllerStub.handle.mockResolvedValue({ statusCode: 200, body: { success: true } });
+    sut = new LogControllerDecorator(controllerStub);
+  });
+
+  it("should call controller handle", async () => {
+    const httpRequest = {
+      body: {
+        email: "any_email",
+        name: "any_name",
+        password: "any_password",
+        passwordConfirmation: "any_password"
+      }
+    };
+    await sut.handle(httpRequest);
+    expect(controllerStub.handle).toHaveBeenCalledWith(httpRequest);
+    expect(controllerStub.handle).toHaveBeenCalledTimes(1);
+  });
+});
