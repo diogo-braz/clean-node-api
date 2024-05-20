0826d8a6b0393d7e5edf616c5ee3645559eacf95
Diogo Braz <diogo.braz04@gmail.com>
2024-05-20 01:04:42 -0300

    test: ensure LogControllerDecorator calls LogErrorRepository if controller returns a server error

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -2,15 +2,19 @@ import { Controller } from "@/presentation/protocols";
 import { LogControllerDecorator } from "./log";
 
 import { mock, MockProxy } from "jest-mock-extended";
+import { serverError } from "@/presentation/helpers/http-helper";
+import { LogErrorRepository } from "@/data/protocols/log-error-repository";
 
 describe("LogController Decorator", () => {
   let sut: LogControllerDecorator;
   let controllerStub: MockProxy<Controller>;
+  let logErrorRepositoryStub: MockProxy<LogErrorRepository>;
 
   beforeEach(() => {
     controllerStub = mock();
+    logErrorRepositoryStub = mock();
     controllerStub.handle.mockResolvedValue({ statusCode: 200, body: { success: true } });
-    sut = new LogControllerDecorator(controllerStub);
+    sut = new LogControllerDecorator(controllerStub, logErrorRepositoryStub);
   });
 
   it("should call controller handle", async () => {
@@ -38,5 +42,22 @@ describe("LogController Decorator", () => {
     };
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse).toEqual({ statusCode: 200, body: { success: true } });
+  });
+
+  it("should call LogErrorRepository with correct error if controller returns a server error", async () => {
+    const fakeError = new Error();
+    fakeError.stack = "any_stack";
+    const error = serverError(fakeError);
+    controllerStub.handle.mockResolvedValueOnce(error);
+    const httpRequest = {
+      body: {
+        email: "any_email",
+        name: "any_name",
+        password: "any_password",
+        passwordConfirmation: "any_password"
+      }
+    };
+    await sut.handle(httpRequest);
+    expect(logErrorRepositoryStub.log).toHaveBeenCalledWith("any_stack");
   });
 });
