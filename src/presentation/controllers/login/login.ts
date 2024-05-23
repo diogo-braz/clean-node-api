616d7792e9548c500467277d04e4c47a984eeb14
Diogo Braz <diogo.braz04@gmail.com>
2024-05-23 09:15:37 -0300

    feat: ensure LoginController returns 500 if EmailValidator throws

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,4 +1,4 @@
-import { badRequest } from "../../../presentation/helpers/http-helper";
+import { badRequest, serverError } from "../../../presentation/helpers/http-helper";
 import { Controller, EmailValidator, HttpRequest, HttpResponse } from "../signup/signup-protocols";
 import { InvalidParamError, MissingParamError } from "../../../presentation/errors";
 
@@ -6,17 +6,22 @@ export class LoginController implements Controller {
   constructor (private readonly emailValidator: EmailValidator) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email } = httpRequest.body;
+    try {
+      const { email } = httpRequest.body;
 
-    if (!email) {
-      return badRequest(new MissingParamError("email"));
+      if (!email) {
+        return badRequest(new MissingParamError("email"));
+      }
+
+      const validEmail = this.emailValidator.isValid(email);
+      if (!validEmail) {
+        return badRequest(new InvalidParamError("email"));
+      }
+
+      return badRequest(new MissingParamError("password"));
+    } catch (error) {
+      return serverError(error as Error);
     }
 
-    const validEmail = this.emailValidator.isValid(email);
-    if (!validEmail) {
-      return badRequest(new InvalidParamError("email"));
-    }
-
-    return badRequest(new MissingParamError("password"));
   }
 }
