6bc24c2b901cf97a4aacbc01b6eb180a58aa6f2d
Diogo Braz <diogo.braz04@gmail.com>
2024-05-23 08:06:55 -0300

    test: ensure LoginController calls EmailValidator with correct email

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,12 +1,16 @@
 import { LoginController } from "./login";
 import { badRequest } from "../../helpers/http-helper";
 import { MissingParamError } from "../../../presentation/errors";
+import { EmailValidator } from "../signup/signup-protocols";
+import { MockProxy, mock } from "jest-mock-extended";
 
 describe("Login Controller", () => {
+  let emailValidatorStub: MockProxy<EmailValidator>;
   let sut: LoginController;
 
   beforeEach(() => {
-    sut = new LoginController();
+    emailValidatorStub = mock();
+    sut = new LoginController(emailValidatorStub);
   });
 
   it("should return 400 if no email is provided", async () => {
@@ -27,5 +31,16 @@ describe("Login Controller", () => {
     };
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse).toEqual(badRequest(new MissingParamError("password")));
+  });
+
+  it("should call EmailValidator with correct email", async () => {
+    const httpRequest = {
+      body: {
+        email: "any_email@mail.com",
+        password: "any_password"
+      }
+    };
+    await sut.handle(httpRequest);
+    expect(emailValidatorStub.isValid).toHaveBeenCalledWith("any_email@mail.com");
   });
 });
