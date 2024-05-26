5065d40233cd3f22959cd0819966b0b719aec474
Diogo Braz <diogo.braz04@gmail.com>
2024-05-26 01:13:32 -0300

    refactor: move sut creation to before each method

--- src/presentation/helpers/validators/required-field-validations.spec.ts
+++ src/presentation/helpers/validators/required-field-validations.spec.ts
@@ -2,14 +2,18 @@ import { MissingParamError } from "../../../presentation/errors";
 import { RequiredFieldValidation } from "./required-field-validation";
 
 describe("RequiredField Validation", () => {
+  let sut: RequiredFieldValidation;
+
+  beforeEach(() => {
+    sut = new RequiredFieldValidation("field");
+  });
+
   it("should return a MissingParamError if validation fails", () => {
-    const sut = new RequiredFieldValidation("field");
     const error = sut.validate({ name: "any_name" });
     expect(error).toEqual(new MissingParamError("field"));
   });
 
   it("should return null if validation succeeds", () => {
-    const sut = new RequiredFieldValidation("field");
     const error = sut.validate({ field: "any_field" });
     expect(error).toBeFalsy();
   });
