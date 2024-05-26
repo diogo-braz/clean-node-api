8c18bcc8a2cee0d1cb2841a2eb1b2e2175a8c657
Diogo Braz <diogo.braz04@gmail.com>
2024-05-26 01:18:31 -0300

    test: ensure CompareFieldsValidation return a InvalidParamError if validation fails

---
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,15 @@
+import { InvalidParamError } from "../../../presentation/errors";
+import { CompareFieldsValidation } from "./compare-fields-validation";
+
+describe("CompareFields Validation", () => {
+  let sut: CompareFieldsValidation;
+
+  beforeEach(() => {
+    sut = new CompareFieldsValidation("field", "fieldToCompare");
+  });
+
+  it("should return a InvalidParamError if validation fails", () => {
+    const error = sut.validate({ field: "any_field", fieldToCompare: "other_field" });
+    expect(error).toEqual(new InvalidParamError("fieldToCompare"));
+  });
+});
