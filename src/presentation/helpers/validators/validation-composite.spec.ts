51babc317c7ff1cbd5cccb46908f1beadc8ef2ae
Diogo Braz <diogo.braz04@gmail.com>
2024-05-26 01:30:47 -0300

    refactor: move sut creation to before each method

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -5,15 +5,16 @@ import { Validation } from "./validation";
 
 describe("Validation Composite", () => {
   let validationStub: MockProxy<Validation>;
+  let sut: ValidationComposite;
 
   beforeEach(() => {
     validationStub = mock();
     validationStub.validate.mockReturnValue(null);
+    sut = new ValidationComposite([validationStub]);
   });
 
   it("should return an error if any validation fails", () => {
     validationStub.validate.mockReturnValueOnce(new MissingParamError("field"));
-    const sut = new ValidationComposite([validationStub]);
     const error = sut.validate({ field: "any_value" });
     expect(error).toEqual(new MissingParamError("field"));
   });
