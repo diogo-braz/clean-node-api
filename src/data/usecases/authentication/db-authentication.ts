00316cc94142caf55aa35f3070b101dbb79adccd
Diogo Braz <diogo.braz04@gmail.com>
2024-06-07 01:14:51 -0300

    feat: ensure DbAuthentication calls UpdateAccessTokenRepository with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -2,12 +2,14 @@ import { LoadAccountByEmailRepository } from "../../../data/protocols/db/load-ac
 import { Authentication, AuthenticationModel } from "../../../data/protocols/authentication";
 import { HashComparer } from "../../../data/protocols/cryptography/hash-comparer";
 import { TokenGenerator } from "../../../data/protocols/cryptography/token-generator";
+import { UpdateAccessTokenRepository } from "../../../data/protocols/db/update-access-token-repository";
 
 export class DbAuthentication implements Authentication {
   constructor (
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
     private readonly hashComparer: HashComparer,
     private readonly tokenGenerator: TokenGenerator,
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository,
   ) {}
 
   async auth (authentication: AuthenticationModel): Promise<string | null> {
@@ -16,6 +18,7 @@ export class DbAuthentication implements Authentication {
       const isValid = await this.hashComparer.compare(authentication.password, account.password);
       if (isValid) {
         const accessToken = await this.tokenGenerator.generate(account.id);
+        await this.updateAccessTokenRepository.update(account.id, accessToken);
         return accessToken;
       }
     }
