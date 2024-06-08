6fff21be480022f2910faf46072dd7b71d868494
Diogo Braz <diogo.braz04@gmail.com>
2024-06-08 04:03:33 -0300

    feat: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,12 +1,20 @@
+import { LoadAccountByEmailRepository } from "../../../../data/protocols/db/load-account-by-email-repository";
 import { AddAccountRepository } from "../../../../data/protocols/db/add-account-repository";
 import { AddAccount } from "../../../../domain/usecases/add-account";
 import { MongoDbHelper } from "../helpers/mongodb-helper";
 import { IdMapper } from "../mappers/id-mapper";
+import { AccountEntity } from "../../../../domain/entities/account";
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (account: AddAccount.Params): Promise<AddAccount.Result> {
     const accountCollection = MongoDbHelper.getCollection("accounts");
     await accountCollection.insertOne(account);
     return IdMapper.toDomain(account);
+  }
+
+  async loadByEmail (email: string): Promise<AccountEntity | null> {
+    const accountCollection = MongoDbHelper.getCollection("accounts");
+    const account = await accountCollection.findOne({ email });
+    return account && IdMapper.toDomain(account);
   }
 }
