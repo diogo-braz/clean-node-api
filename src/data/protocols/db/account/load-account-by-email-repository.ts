4d74e25c2879de60310c80dde23e4a28abe34b12
Diogo Braz <diogo.braz04@gmail.com>
2024-06-08 05:28:27 -0300

    fix: resolve import

--- src/data/protocols/db/account/load-account-by-email-repository.ts
+++ src/data/protocols/db/account/load-account-by-email-repository.ts
@@ -1,4 +1,4 @@
-import { AccountEntity } from "../../../domain/entities/account";
+import { AccountEntity } from "../../../../domain/entities/account";
 
 export  interface LoadAccountByEmailRepository {
   loadByEmail (email: string): Promise<AccountEntity | null>
